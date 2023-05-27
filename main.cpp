e746ce6f8b6505fb04b11ccef1a26e2f63416e3e
balint <balint.soczo@gmail.com>
2023-05-27 19:27:20 +0200

    Perf mérés 1. verzió, 32 bit alatt 4.5mp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@
 
 
 int main() {
-	
-	const control_panel cp;
+	const std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+
+	constexpr control_panel cp;
 	cp.initiate_analysis();
 	std::cout << "Analysis complete" << std::endl;
-	
+
+	const std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+	std::cout << "Time elapsed: " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << " milliseconds" << std::endl;
 }
